8a0f2bacc92f011209dd4caf7ce0180f06dfc81c
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2023-11-18 11:19:34 +0600

    Done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 // const port = 3000;
 
@@ -7,10 +8,16 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+// api/v1/students/create-student
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
   const a = 10;
 
   res.send(a);
-});
+};
+
+app.get('/', getAController);
 
 export default app;
