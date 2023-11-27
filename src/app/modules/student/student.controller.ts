066e96ab2723d3da40a5a9acab0141643bb82ad2
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2023-11-28 00:09:29 +0600

    validation

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,15 +1,9 @@
 import httpStatus from 'http-status';
-import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { StudentServices } from './student.service';
 import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
 
-const catchAsync = (fn: RequestHandler) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-  };
-};
-
-const getSingleStudent = catchAsync(async (req, res, next) => {
+const getSingleStudent = catchAsync(async (req, res) => {
   const { studentId } = req.params;
   const result = await StudentServices.getSingleStudentsFromDB(studentId);
   sendResponse(res, {
@@ -20,7 +14,7 @@ const getSingleStudent = catchAsync(async (req, res, next) => {
   });
 });
 
-const getAllStudents: RequestHandler = catchAsync(async (req, res, next) => {
+const getAllStudents = catchAsync(async (req, res) => {
   const result = await StudentServices.getAllStudentsFromDB();
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -29,21 +23,17 @@ const getAllStudents: RequestHandler = catchAsync(async (req, res, next) => {
     data: result,
   });
 });
-const deleteStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { studentId } = req.params;
-    const result = await StudentServices.deleteStudentFromDB(studentId);
+const deleteStudent = catchAsync(async (req, res) => {
+  const { studentId } = req.params;
+  const result = await StudentServices.deleteStudentFromDB(studentId);
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is deleted successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is deleted successfully',
+    data: result,
+  });
+});
 
 export const StudentControllers = {
   getAllStudents,
--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -33,29 +33,34 @@ const localGuardianValidationSchema = z.object({
   address: z.string().min(1, { message: 'Address is required' }),
 });
 
-const studentValidationSchema = z.object({
-  id: z.string().min(1, { message: 'ID is required' }),
-  password: z.string().max(20),
-  name: userNameValidationSchema,
-  gender: z.enum(['male', 'female', 'other']),
-  dateOfBirth: z.string().optional(),
-  email: z.string().email({ message: 'Email is not a valid email type' }),
-  contactNo: z.string().min(1, { message: 'Contact No is required' }),
-  emergencyContactNo: z
-    .string()
-    .min(1, { message: 'Emergency Contact No is required' }),
-  bloodGroup: z
-    .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])
-    .optional(),
-  presentAddress: z.string().min(1, { message: 'Present Address is required' }),
-  permanentAddress: z
-    .string()
-    .min(1, { message: 'Permanent Address is required' }),
-  guardian: guardianValidationSchema,
-  localGuardian: localGuardianValidationSchema,
-  profileImg: z.string().optional(),
-  isActive: z.enum(['active', 'blocked']).default('active'),
-  isDeleted: z.boolean(),
+const createStudentValidationSchema = z.object({
+  body: z.object({
+    password: z.string().max(20),
+    student: z.object({
+      name: userNameValidationSchema,
+      gender: z.enum(['male', 'female', 'other']),
+      dateOfBirth: z.string().optional(),
+      email: z.string().email({ message: 'Email is not a valid email type' }),
+      contactNo: z.string().min(1, { message: 'Contact No is required' }),
+      emergencyContactNo: z
+        .string()
+        .min(1, { message: 'Emergency Contact No is required' }),
+      bloodGroup: z
+        .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])
+        .optional(),
+      presentAddress: z
+        .string()
+        .min(1, { message: 'Present Address is required' }),
+      permanentAddress: z
+        .string()
+        .min(1, { message: 'Permanent Address is required' }),
+      guardian: guardianValidationSchema,
+      localGuardian: localGuardianValidationSchema,
+      profileImg: z.string().optional(),
+    }),
+  }),
 });
 
-export default studentValidationSchema;
+export const studentValidations = {
+  createStudentValidationSchema,
+};
